2202162d12a52df8438d7cb8c5ffc37847fd87c4
sinku <sininoir@gmail.com>
2022-01-05 01:00:29 +0330

    Add image loading

--- tools.cpp
+++ tools.cpp
@@ -5,3 +5,47 @@ using namespace std;
 void sdl_error(string error_message) {
     cout << error_message << endl << "SDL_Error: " << SDL_GetError() << endl;
 }
+
+void sdl_image_error(std::string error_message) {
+    cout << error_message << endl << "SDL_Error: " << IMG_GetError() << endl;
+}
+
+SDL_Surface* imageSurfaceRead(std::string path, SDL_Surface *window_surface) {
+	SDL_Surface* optimizedSurface = NULL;
+
+	SDL_Surface* loadedSurface = IMG_Load(path.c_str());
+	if(loadedSurface == NULL) {
+        sdl_image_error("Unable to load image");
+        return NULL;
+	}
+
+    optimizedSurface = SDL_ConvertSurface( loadedSurface, window_surface->format, 0 );
+    if( optimizedSurface == NULL ) {
+        sdl_error("Unable to optimize image");
+        return loadedSurface;
+    }
+
+    SDL_FreeSurface( loadedSurface );
+
+	return optimizedSurface;
+}
+
+SDL_Texture* imageTextureRead(std::string path, SDL_Renderer *renderer) {
+	SDL_Texture* optimizedSurface = NULL;
+
+	SDL_Surface* loadedSurface = IMG_Load(path.c_str());
+	if(loadedSurface == NULL) {
+        sdl_image_error("Unable to load image");
+        return NULL;
+	}
+
+    optimizedSurface = SDL_CreateTextureFromSurface(renderer, loadedSurface);
+    if( optimizedSurface == NULL ) {
+        sdl_error("Unable to create texture");
+        return NULL;
+    }
+
+    SDL_FreeSurface( loadedSurface );
+
+	return optimizedSurface;
+}
--- tools.hpp
+++ tools.hpp
@@ -4,8 +4,13 @@
 #include <string>
 #include <iostream>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
 
 void sdl_error(std::string error_message);
+void sdl_image_error(std::string error_message);
+SDL_Surface* imageSurfaceRead(std::string path, SDL_Surface *window_surface);
+SDL_Texture* imageTextureRead(std::string path, SDL_Renderer *renderer);
 // TODO: Add loadimage
 
 #endif
