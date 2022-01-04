cf15caae3baf89d2e58b6217542c0a544bbe011c
sinku <sininoir@gmail.com>
2022-01-04 16:06:42 +0330

    Add buttons

--- component.cpp
+++ component.cpp
@@ -1,1 +1,36 @@
 #include "component.hpp"
+
+using namespace std;
+
+Button::Button(SDL_Rect draw_rect, std::string text) {
+    draw_rect_ = draw_rect;
+    text_ = text;
+}
+
+void Button::checkClicked(SDL_Event *evt) {
+    if(evt->type == SDL_MOUSEBUTTONDOWN) {
+        if(evt->button.button == SDL_BUTTON_LEFT &&
+                evt->button.x >= draw_rect_.x &&
+                evt->button.x <= (draw_rect_.x + draw_rect_.w) &&
+                evt->button.y >= draw_rect_.y &&
+                evt->button.y <= (draw_rect_.y + draw_rect_.h)) {
+            clicked = true;
+            cout<<"clicked"<<endl;
+        }
+    }
+    if (evt->type == SDL_MOUSEBUTTONUP) {
+        cout<<"released!"<<endl;
+        clicked = false;
+    }
+}
+
+void Button::render(SDL_Renderer *renderer) {
+    // TODO: ...
+    if (clicked) {
+        SDL_SetRenderDrawColor(renderer, 100, 200, 255, 150);
+    }else {
+        SDL_SetRenderDrawColor(renderer, 100, 200, 255, 255);
+    }
+    
+    SDL_RenderFillRect(renderer, &draw_rect_);
+}
--- component.hpp
+++ component.hpp
@@ -1,7 +1,22 @@
 #ifndef _COMPONENT_HPP
 #define _ENGINE_HPP
 
+#include <SDL2/SDL.h>
+#include <iostream>
+#include <string>
 
+
+class Button {
+    public:
+    Button(SDL_Rect draw_rect, std::string text);
+    void checkClicked(SDL_Event *evt);
+    void render(SDL_Renderer *renderer);
+
+    private:
+    SDL_Rect draw_rect_;
+    std::string text_;
+    bool clicked;
+};
 
 
 #endif
